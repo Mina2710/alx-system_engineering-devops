16f06d99a402991c1f6eb0843aecc263ebac687f
Mina2710 <125763571+Mina2710@users.noreply.github.com>
2023-11-26 18:06:56 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
-# install_flask.pp
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/bin',
-  refreshonly => true,
-  creates     => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
-}
-
-file { '/usr/local/bin/flask':
-  ensure  => link,
-  target  => '/usr/local/bin/flask',
-  require => Exec['install_flask'],
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
