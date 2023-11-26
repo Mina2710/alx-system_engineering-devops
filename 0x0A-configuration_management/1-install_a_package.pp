40a2167d5a4ef9746efb29644df131b98764cf4e
Mina2710 <125763571+Mina2710@users.noreply.github.com>
2023-11-26 18:04:47 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {
-  'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/bin',
+  refreshonly => true,
+  creates     => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+}
+
+file { '/usr/local/bin/flask':
+  ensure  => link,
+  target  => '/usr/local/bin/flask',
+  require => Exec['install_flask'],
 }
